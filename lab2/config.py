c0acfaebc942d4ed23c2c15b17837f85515f2911
1172510217 <2584363094@qq.com>
2019-12-12 23:09:19 +0800

    增加配置文件，减少了模块变量的数目

---
+++ lab2/config.py
@@ -0,0 +1,10 @@
+Categories = ['整体', '使用体验', '功效', '价格', '物流', '气味', '包装', '真伪', '服务', '成分', '其他',
+              '尺寸', '新鲜度']
+Label_Lst = ['O', 'B-ASP', 'I-ASP', 'B-OPI', 'I-OPI']
+Polarity = ['中性', '正面', '负面']
+Train_Reviews = '../source/train_reviews.csv'  # 电商评论训练集源文件
+Train_Labels = '../source/train_labels.csv'  # 电商评论训练集结果文件
+Test_Reviews = '../source/test_reviews.csv'  # 电商评论测试集源文件
+Answer_1 = '../answer/task1_answer.csv'
+Answer_2 = '../answer/task2_answer.csv'
+Answer_3 = '../answer/task3_answer.csv'
